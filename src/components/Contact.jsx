8502c9b9410b5a059f1704def45cabd429d0ab05
kishorMR <kishorgowda435@gmail.com>
2026-06-08 00:02:28 +0530

    changed email service

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,15 +26,15 @@ export default function Contact() {
 
     try {
       await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        "service_r7fiykk",   // replace with your Service ID
+        "template_1jri6qn",  // replace with your Template ID
         {
           name: formData.name,
           email: formData.email,
           subject: formData.subject,
           message: formData.message,
         },
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        "bNVGInDQplOkahOAG"    // replace with your Public Key
       );
 
       alert("Message Sent Successfully!");
