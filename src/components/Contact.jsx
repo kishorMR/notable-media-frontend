890a47a0e4e43ee72759839e576faf5001a93c3a
kishorMR <kishorgowda435@gmail.com>
2026-06-07 18:16:11 +0530

    made changes in env file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,7 +22,7 @@ export default function Contact() {
 
     try {
       const response = await fetch(
-        `${process.env.REACT_APP_API_URL}/api/contact`,
+        `${import.meta.env.VITE_API_URL}/api/contact`,
         {
           method: "POST",
           headers: {
